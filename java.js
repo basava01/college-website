9d397c17ad8648928abdbb51dbc602dbf372e357
basavaraj <basavrajt556@gmail com>
2024-12-08 14:34:40 +0530

    added a branch

--- java.js
+++ java.js
@@ -1,4 +1,3 @@
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
-    alert('Thank you for contacting us, ' + document.getElementById('name').value + '!');
-});
+    alert('Thank you for contacting us, ' + document.getElementById('name').value + '!'
